5daf0cdde235be4c9cda60b7345f9a896bd162ca
Guillaume <gillard.guillaume@gmail.com>
2023-10-15 12:24:32 +0200

    final version1

--- Devoir 1/Devoir1.py
+++ Devoir 1/Devoir1.py
@@ -1,22 +1,14 @@
-#import numpy
-
+# 15/10/2023 Guillaume Gillard
 
 def devoir1(trajets_train, durees_train, ville_depart, durees_voldirect, sacrifice):
-
-    # TO DO
-
-    # Train 
-    ville = "bxl"
+   #---- Train ----#
     N = []
     Ns = [ville_depart]
     N_s = []
     graph = {}
     smallest_dist_train = {}
-    smallest_dist_plane = {}
-    count_train = 0
     for journey, duration in zip (trajets_train, durees_train):
-        cities = tuple(journey.split("-"))
-        city1, city2 = cities 
+        city1, city2 = tuple(journey.split("-"))
         if city1 not in smallest_dist_train :
             smallest_dist_train[city1] = float("infinity")
         if city2 not in smallest_dist_train :
@@ -35,10 +27,9 @@ def devoir1(trajets_train, durees_train, ville_depart, durees_voldirect, sacrifi
             graph[city2][city1] = duration
         else :
             graph[city2] = {city1: duration}
-    #print(graph) 
     smallest_dist_train[ville_depart] = 0
     N_s = [x for x in N if x not in Ns]
-    #initialazing s-d-t
+    #initialazing smallest_dist_train
     for city, cost in graph[ville_depart].items():
         smallest_dist_train[city] = cost + 30
     while N_s != []:
@@ -48,28 +39,14 @@ def devoir1(trajets_train, durees_train, ville_depart, durees_voldirect, sacrifi
                     smallest_dist_train[connexion] = smallest_dist_train[city] + 15 + cost
         Ns.append(city)
         N_s.remove(city)
-    print(smallest_dist_train)
-        
-
-    return ville, smallest_dist_train, smallest_dist_plane, count_train
-
-
-
-trajets_train = ["BRU-PAR", "BRU-AMS", "BRU-LON", "BRU-COL", "PAR-BOR", "PAR-LYO", "PAR-FRA", "PAR-LON", "PAR-REN",
-                 "AMS-BER", "AMS-COL", "AMS-HAM", "COL-HAM", "COL-FRA", "COL-BER", "LYO-MAR", "LYO-ZUR", "FRA-HAM",
-                 "FRA-BER", "FRA-MUN", "FRA-ZUR", "BER-MUN", "BER-HAM", "BER-PRA", "MUN-ZUR", "MIL-LYO", "MIL-ZUR",
-                 "LYO-BAR", "BAR-MAR", "BOR-TLS", "TLS-BAR", "TLS-MAR", "LON-BIR", "LON-MAN", "MAN-BIR", "MAN-EDI",
-                 "EDI-GLW", "LYO-TLS", "HAM-COP", "PAR-TLS"]
-
-durees_train = [80, 95, 120, 105, 120, 110, 230, 140, 90, 385, 180, 300, 215, 60, 280, 110,
-240, 280, 275, 200, 235, 255, 175, 265, 210, 270, 240, 330, 285, 120, 195, 225, 95, 140, 80, 190,
-50, 250, 280, 260]
-
-ville_depart = "BRU"
-
-durees_voldirect = [65, 120, 85, 80, 100, 60, 90, 100, 60, 105, 75, 80, 85, 85, 100, 90, 80,
-65, 90, 95, 100, 75]
-
-sacrifice = 60
-
-devoir1(trajets_train, durees_train, ville_depart, durees_voldirect, sacrifice)
+    del smallest_dist_train[ville_depart]
+    smallest_dist_train = {key: smallest_dist_train[key] for key in sorted(smallest_dist_train)}
+    ville = list(smallest_dist_train.keys())
+    min_distances_train = list(smallest_dist_train.values())
+    min_distances_avion = [x + 180 for x in durees_voldirect]
+    #---- checking plane vs train ----#
+    count_train = 0
+    for x, y in zip(min_distances_train, min_distances_avion):
+        if x <= y + sacrifice:
+            count_train += 1      
+    return ville, min_distances_train, min_distances_avion, count_train
